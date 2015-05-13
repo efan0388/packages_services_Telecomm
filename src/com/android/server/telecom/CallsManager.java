CyanogenMod/android_packages_services_Telecomm

Telecomm : Add ability to block calls if spam
Change-Id: If11ceebe9e4d1ae5e447caddbb053f852d7a3b06

src/com/android/server/telecom/CallsManager.java
@@ -85,6 +85,7 @@ public final class CallsManager extends Call.ListenerBase {
         void onVideoStateChanged(Call call);
         void onCallSubstateChanged(Call call);
         void onCanAddCallChanged(boolean canAddCall);
+        void onProcessIncomingCall(Call call);
     }
 
     /**
@@ -138,6 +139,7 @@ public final class CallsManager extends Call.ListenerBase {
     private final PhoneAccountRegistrar mPhoneAccountRegistrar;
     private final MissedCallNotifier mMissedCallNotifier;
     private final BlacklistCallNotifier mBlacklistCallNotifier;
+    private final CallerInfoBlocker mCallerInfoBlocker;
     private final Set<Call> mLocallyDisconnectingCalls = new HashSet<>();
 
     private boolean mCanAddCall = true;
@@ -202,6 +204,7 @@ public final class CallsManager extends Call.ListenerBase {
         mDtmfLocalTonePlayer = new DtmfLocalTonePlayer(context);
         mConnectionServiceRepository = new ConnectionServiceRepository(mPhoneAccountRegistrar,
                 context);
+        mCallerInfoBlocker = new CallerInfoBlocker(context);
 
         mListeners.add(statusBarNotifier);
         mListeners.add(mCallLogManager);
@@ -216,6 +219,7 @@ public final class CallsManager extends Call.ListenerBase {
         mListeners.add(mHeadsetMediaButton);
         mListeners.add(RespondViaSmsManager.getInstance());
         mListeners.add(mProximitySensorManager);
+        mListeners.add(mCallerInfoBlocker);
     }
 
     @Override
@@ -249,6 +253,12 @@ public final class CallsManager extends Call.ListenerBase {
         Log.d(this, "onSuccessfulIncomingCall");
 
         if (isCallBlacklisted(incomingCall)) {
+            mCallLogManager.logCall(incomingCall, Calls.BLACKLIST_TYPE);
+            incomingCall.setDisconnectCause(
+                    new DisconnectCause(android.telephony.DisconnectCause.CALL_BLACKLISTED));
+        } else if (mCallerInfoBlocker.shouldBlock(incomingCall.getNumber())) {
+            // TODO: show notification for blocked spam calls
+            // TODO: add unique call type for spam
             mCallLogManager.logCall(incomingCall, Calls.BLACKLIST_TYPE);
             incomingCall.setDisconnectCause(
                     new DisconnectCause(android.telephony.DisconnectCause.CALL_BLACKLISTED));
@@ -407,6 +417,9 @@ public final class CallsManager extends Call.ListenerBase {
         call.setExtras(extras);
         // TODO: Move this to be a part of addCall()
         call.addListener(this);
+        for (CallsManagerListener listener : mListeners) {
+            listener.onProcessIncomingCall(call);
+        }
         call.startCreateConnection(mPhoneAccountRegistrar);
     }
 

efan0388/packages_services_Telecomm

src/com/android/server/telecom/CallsManager.java
@@ -77,6 +77,7 @@ public final class CallsManager extends Call.ListenerBase {
         void onIsVoipAudioModeChanged(Call call);
         void onVideoStateChanged(Call call);
         void onCanAddCallChanged(boolean canAddCall);
+        void onProcessIncomingCall(Call call);
     }
 
     /**
@@ -129,6 +130,7 @@ public final class CallsManager extends Call.ListenerBase {
     private final PhoneAccountRegistrar mPhoneAccountRegistrar;
     private final MissedCallNotifier mMissedCallNotifier;
     private final BlacklistCallNotifier mBlacklistCallNotifier;
+    private final SpamBlocker mCallerInfoBlocker;
     private final Set<Call> mLocallyDisconnectingCalls = new HashSet<>();
     private final Set<Call> mPendingCallsToDisconnect = new HashSet<>();
     /* Handler tied to thread in which CallManager was initialized. */
@@ -183,6 +185,7 @@ public final class CallsManager extends Call.ListenerBase {
         mConnectionServiceRepository = new ConnectionServiceRepository(mPhoneAccountRegistrar,
                 context);
         mInCallWakeLockController = new InCallWakeLockController(context, this);
+        mCallerInfoBlocker = new SpamBlocker(context);
 
         mListeners.add(statusBarNotifier);
         mListeners.add(mCallLogManager);
@@ -197,6 +200,7 @@ public final class CallsManager extends Call.ListenerBase {
         mListeners.add(mHeadsetMediaButton);
         mListeners.add(RespondViaSmsManager.getInstance());
         mListeners.add(mProximitySensorManager);
+        mListeners.add(mCallerInfoBlocker);
     }
 
     @Override
@@ -230,6 +234,12 @@ public final class CallsManager extends Call.ListenerBase {
         Log.d(this, "onSuccessfulIncomingCall");
 
         if (isCallBlacklisted(incomingCall)) {
+            mCallLogManager.logCall(incomingCall, Calls.BLACKLIST_TYPE);
+            incomingCall.setDisconnectCause(
+                    new DisconnectCause(android.telephony.DisconnectCause.CALL_BLACKLISTED));
+        } else if (mCallerInfoBlocker.shouldBlock(incomingCall.getNumber())) {
+            // TODO: show notification for blocked spam calls
+            // TODO: add unique call type for spam
             mCallLogManager.logCall(incomingCall, Calls.BLACKLIST_TYPE);
             incomingCall.setDisconnectCause(
                     new DisconnectCause(android.telephony.DisconnectCause.CALL_BLACKLISTED));
@@ -438,6 +448,9 @@ public final class CallsManager extends Call.ListenerBase {
         call.setExtras(extras);
         // TODO: Move this to be a part of addCall()
         call.addListener(this);
+        for (CallsManagerListener listener : mListeners) {
+            listener.onProcessIncomingCall(call);
+        }
         call.startCreateConnection(mPhoneAccountRegistrar);
     }
 